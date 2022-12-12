6798271a47263c18bbdfabddaf465b6a667622ab
“HMM” <huangmm.mail@gmail.com>
2022-12-12 20:39:40 +0800

    20221212

--- chapter03/src/main/java/org/chapter03/Application03.java
+++ chapter03/src/main/java/org/chapter03/Application03.java
@@ -1,4 +1,13 @@
 package org.chapter03;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application03 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application03.class, args);
+    }
+
 }
